9ea3fcd8507fadd4b122f623c56b2764b51f7609
Kathleen Santos <kathleen.santos@csacademy.com.br>
2025-03-23 11:44:51 -0300

    create dashboard page

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,9 +1,13 @@
+import Header from "@/components/Header";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
   return (
-    <div>
+    <div className="flex min-h-screen flex-col antialiased">
+      <Header />
+      <div className="flex flex-1 flex-col gap-4 p-8 pt-6">
       <Outlet />
+      </div>
     </div>
   );
 };
